4cf71b45a6fa6afdb162741c04077e3dba5e94aa
Vishal <vishalpraneeth7771@gmail.com>
2023-02-12 15:23:59 +0530

    main.go

---
+++ 02variable/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Variables in Go")
+	var username string = "Vishal"
+	fmt.Println("Username is", username)
+	fmt.Printf("Variable type is: %T \n", username)
+
+	var isLoggedin bool = true
+	fmt.Println(isLoggedin)
+	fmt.Printf("Variable type is: %T \n", isLoggedin)
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable type is: %T \n", anotherVariable)
+
+	var website = "www.google.com"
+	fmt.Println(website)
+
+	numberOfUsers := 100
+	fmt.Println(numberOfUsers)
+	
+}
